8d43c555313a9af4769086aaf2a32c8567104d90
Alexandre <em_alexandre@yahoo.co.jp>
2021-03-22 17:56:41 +0100

    init api

---
+++ app/main/config.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE_URI_DEV = os.getenv('DATABASE_URI_DEV')
+DATABASE_URI_PROD = os.getenv('DATABASE_URI_PROD')
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI_DEV
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+    # select inet_server_addr( ), inet_server_port( ); <- to show the ip address of the server and its port
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI_DEV
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI_PROD
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
+
+key = Config.SECRET_KEY
